dfb8c7792e54ffd239bc06d1204f2914df34b0cd
hri1 <97541156+hri1@users.noreply.github.com>
2026-07-25 06:19:58 +0530

    Update AWS instance AMI and instance type

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -41,8 +41,8 @@ resource "aws_security_group" "ec2_security_group" {
 }
 
 resource "aws_instance" "Monitoring_server" {
-ami = "ami-00bb6a80f01f03502"  
-instance_type = "t2.medium"
+ami = "ami-0b6d9d3d33ba97d99"  
+instance_type = "c7i-flex.large"
 security_groups = [aws_security_group.ec2_security_group.name]
 key_name = var.key_name
 tags = {
